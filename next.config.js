9b125dfc42d642274659325177a95b1c4173e00e
muharremsen <info@muharremsen.com>
2025-12-30 00:07:22 +0300

    Fix: next.config.js trailing slash ayarÄ± eklendi - redirect hatasÄ± dÃ¼zeltmesi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
+  async redirects() {
+    return [];
+  },
 };
 
 module.exports = nextConfig;
